3645e04b550839965a1c2a2c982a72aaf0ae1201
Nabeel <Ghulam.Nabeel@sony.com>
2023-02-04 13:22:50 +0530

    Home/avatar page Styling

--- pages/choose-avatar/index.tsx
+++ pages/choose-avatar/index.tsx
@@ -16,32 +16,26 @@ const ChooseAvatar = () => {
         <>
             <Header />
             <div className="flex flex-col m-4">
-                <div className="avatar-container flex justify-center p-4">
+                <div className="avatar-container flex rounded-lg justify-center p-4">
                     <div className="flex-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
-                        <img className="rounded-t-lg" src="../images/man.png" alt="" />
-                        <div className="p-5">
-                            <h5>Avatar Name</h5>
-                            <div className="text-center m-2">
-                                <button onClick={()=>handleClick('man')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Choose</button>
-                            </div>
+                        <img className="rounded-t-lg w-full h-80 object-cover" src="../images/man.png" alt="" />
+                        <div className="p-5 flex justify-between items-center">
+                            <span>John Doe</span>
+                            <button onClick={()=>handleClick('man')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Choose</button> 
                         </div>
                     </div>
                     <div className="flex-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
-                        <img className="rounded-t-lg" src="../images/man2.png" alt="" />
-                        <div className="p-5">
-                            <h5>Avatar Name</h5>
-                            <div className="text-center m-2">
-                                <button onClick={()=>handleClick('man2')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Choose</button>
-                            </div>
+                        <img className="rounded-t-lg w-full h-80 object-cover" src="../images/man2.png" alt="" />
+                        <div className="p-5 flex justify-between items-center">
+                            <span>Bill Gates</span>
+                            <button onClick={()=>handleClick('man2')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Choose</button>
                         </div>
                     </div>
                     <div className="flex-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
-                        <img className="rounded-t-lg" src="../images/woman.png" alt="" />
-                        <div className="p-5">
-                            <h5>Avatar Name</h5>
-                            <div className="text-center m-2">
-                                <button onClick={()=>handleClick('woman')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Choose</button>
-                            </div>
+                        <img className="rounded-t-lg w-full h-80 object-cover" src="../images/woman.png" alt="" />
+                        <div className="p-5 flex justify-between items-center">
+                            <span>Daisy Williams</span>
+                                <button onClick={()=>handleClick('woman')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Choose</button>                            
                         </div>
                     </div>
                 </div>
